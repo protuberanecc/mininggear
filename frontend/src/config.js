bcf207e7f0136d9cf444f850610805295baf3dda
protuberanecc <painosmax@gmail.com>
2025-03-10 23:18:06 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://127.0.0.1:8000/api/v1/'
-export const BASE_URL = 'http://127.0.0.1:8000/'
+export const BASE_API_URL = 'https://mining-gear-ua.com/api/v1/'
+export const BASE_URL = 'https://mining-gear-ua.com/'
 export const BOT_TOKEN = '7691092342:AAFqUlxyqNjv0IyAX1bvyzq_qrpHoIw8lEg'
 export const TELEGRAM_CHAT_ID = '7905071685'
